184f999693c66913a8cca562ae4c88d842539d84
Austin Eschenbach <austin.eschenbach@gmail.com>
2014-11-21 14:57:00 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,470 @@
+<?php include 'validate.php'; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<meta name="google-site-verification" content="BAUpTQg_KlexBk3nuAE3L_bScvcD3t2AbFfmyOVWg4U" />
+
+		<meta name="description" content="Hi! I'm Austin Eschenbach, a front-end developer based in New York City with a strong with a background in analytics, web marketing, and film/audio production.">
+
+		<title>Austin Eschenbach - Analytics | Development | Design - eschie.info</title>
+			<link rel="canonical" href="http://eschie.info/" />
+			<meta property='og:locale' content='en_US'/>
+			<meta property='og:type' content='website'/>
+			<meta property='og:title' content='Austin Eschenbach - Analytics | Development | Design - eschie.info'/>
+			<meta property='og:url' content='http://eschie.info/'/>
+			<meta property='og:site_name' content='Austin Eschenbach'/>
+			<meta property="og:description" content="Austin Eschenbach is a freelance front-end developer based in New York City with a strong background in analytics, web marketing, and film/audio production."/>
+
+		<link href='http://fonts.googleapis.com/css?family=Istok+Web:300,400,600' rel='stylesheet' type='text/css' />
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/bootstrap-responsive.css">
+		<link rel="shortcut icon" type="image/x-icon" href="http://eschie.info/img/logo2.ico" />
+		<link rel="shortcut icon" href="http://eschie.info/img/logo2.ico" type="image/vnd.microsoft.icon"/>
+		<link rel="icon" href="http://eschie.info/img/logo2.ico" type="image/x-ico"/>
+
+
+		<style type="text/css">
+			body {font-family:"Istok Web";padding-top:40px;}
+			.error{color:red;}
+			.me{background-image: url(http://farm2.staticflickr.com/1247/5159083202_7d726ac662_o.jpg);background-size:cover;background-position: right bottom;}
+			.thumbnail-round {padding: 0;border: 0;-webkit-border-radius: 50%;-moz-border-radius: 50%;border-radius: 50%;}
+			.avatar{padding:10px 0;}
+			.avatar h1{line-height:1em;}
+			.navbar-fixed-top{margin-bottom:0;position:fixed;}
+			.docs{display:inline-block;}
+			@media (max-width: 767px) {
+				.me-container{text-align:center;}
+				.docs{width:100px;margin:0 auto;}
+				.docs > div, .docs a{color:white;font-size:11px;line-height:16px;padding-left:5px;}
+			}
+		</style>
+		<!--[if lt IE 9]>
+			<script src="dist/html5shiv.js"></script>
+		<![endif]-->
+		<!--[if IE]>
+   			<style type="text/css">
+				.me-container { 
+				   background:transparent;
+				   filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#50999999,endColorstr=#50999999); 
+				   zoom: 1;
+				} 
+			</style>
+		<![endif]-->
+	</head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="41">
+		<div class="navbar navbar-inverse navbar-fixed-top" style="position:fixed;">
+			<div class="navbar-inner" style="text-align:center;">
+				<div class="container">
+					<!--<a href="#" class="brand">eschie</a>-->
+
+						<ul class="nav">
+							<li><a href="#profile" class="main-nav"><i class="icon-user icon-white"></i></a></li>
+							<li><a href="#resume" class="main-nav"><i class="icon-file icon-white"></i></a></li>
+							<li><a href="#portfolio" class="main-nav"><i class="icon-briefcase icon-white"></i></a></li>
+							<li><a href="#contact" class="main-nav"><i class="icon-comment icon-white"></i></a></li>
+<!-- 							<li><a href="https://soundcloud.com/eschie/sets/new-1" target="about:blank"><i class="icon-music icon-white"></i></a></li> -->
+						</ul>
+
+				</div>
+			</div>
+		</div>
+
+		<div id="profile" class="me" style="background-color:#333;">
+			<div class="layer" style="width:100%;height:100%;background-color:rgba(0,0,0,.8);">
+				<div class="container me-container">
+					<div class="row-fluid">
+<!-- 						<div class="span2">
+							<br/>
+							<div class="underline" style="color:white;">ME.info</div>
+						</div> -->
+						<div class="span2 avatar" style="color:white;text-align:center;">
+							<img src="/img/eschie3.png" alt="" class="thumbnail-round" width="75"><br/>
+							<h1 style="color:white;font-size: 1em;display:inline;">Austin Eschenbach</h1><br/>
+						</div>
+						<div class="span5 offset1" style="color:white;">
+							<p>I'm Austin Eschenbach, a <strong>Front End Developer</strong> with a background in <strong>analytics</strong>, <strong>web-marketing</strong>, and <strong>film/audio production</strong>.</p>
+						</div>
+						<div class="span3" style="text-align:center;">
+							<div class="docs">
+								<p class="underline" style="color:white;">RESUME</p>
+								<br/><br/>
+								<img class="floatLeft" src="img/wordico.png" style="padding-left:5px;"><a class="floatLeft" href="/docs/Resume2.docx" style="color:white;font-size:11px;line-height:16px;padding-left:5px;" target="about:blank">DOC</a> 
+								<img class="floatLeft" src="img/pdfs2.png" style="padding-left:5px;"><a class ="floatLeft" href="/docs/resume.pdf" style="color:white;font-size:11px;line-height:16px;padding-left:5px;" target="about:blank">PDF</a>
+								<br class="clear">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--PROFILE-->
+
+		<div id="resume" style="height:auto;background-color: white;">
+			<div class="container" style="max-width:1240px;margin:0 auto;">	
+				<br/>
+				<div class="row">
+					<div class="span2">
+						<div class="underline">WORK</div>
+					</div>
+
+					<div class="span5 offset1">
+                        <p>Freelance Web Developer</p>
+                        <p style="font-style: italic;">AWE</p>
+                        <p class="grey">Developed and designed modern web sites for clients and companies on a contractual basis.</p>
+                    </div>
+                    <div class="span2 offset1 grey">
+                        <p>New York, NY</p>
+                        <p>7/2014 - Present</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span5 offset3">
+                        <p>Front End Developer</p>
+                        <p style="font-style: italic;">About.com</p>
+                        <p class="grey">Developed and maintained the About.com legacy platform front-end during transition to the new platform.</p>
+                    </div>
+                    <div class="span2 offset1 grey">
+                        <p>New York, NY</p>
+                        <p>11/2013 - 7/2014</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span5 offset3">
+						<p>Web Developer / Web Marketing Specialist</p>
+						<p style="font-style: italic;">Practising Law Institute</p>
+						<p class="grey">Developed landing pages and mini-sites for marketing campaigns, authored marketing and sales analytics reports, oversaw pay-per-click and display ad campaigns, and composed, filmed, and edited marketing promotional videos.</p>
+					</div>
+					<div class="span2 offset1 grey">
+						<p>New York, NY</p>
+						<p>3/2012 - 6/2013</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span5 offset3">
+						<p>Video Marketing Intern</p>
+						<p style="font-style: italic;">Practising Law Institute</p>
+						<p class="grey">Created specialized informational and promotional video content for use in web marketing campaigns.</p>
+					</div>
+					<div class="span2 offset1 grey">
+						<p>New York, NY</p>
+						<p>10/2011 - 3/2012</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span5 offset3">
+						<p>Content Editor</p>
+						<p style="font-style: italic;">HipHopDNA</p>
+						<p class="grey">Responsible for contributing daily content and planning growth and development through content marketing and social media.</p>
+					</div>
+					<div class="span2 offset1 grey">
+						<p>New York, NY</p>
+						<p>9/2011 - Present</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span5 offset3">
+						<p>Fundraising Ambassador</p>
+						<p style="font-style: italic;">UNC Fundraising</p>
+						<p class="grey">Raised over $60,000 in alumni/corporate contributions by speaking to over 30 prospective donors on a daily basis, while honing valuable sales and cold-calling skills.</p>
+					</div>
+					<div class="span2 offset1 grey">
+						<p>Chapel Hill, NC</p>
+						<p>7/2010 - 7/2011</p>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="span2">
+						<div class="underline">EDUCATION</div>
+					</div>
+					<div class="span5 offset1">
+						<p>University of North Carolina at Chapel Hill</p>
+						<p style="font-style: italic;">Bachelor of Science Psychology</p>
+						<p style="font-style: italic;">Minors History & Latin American Studies</p>
+					</div>
+					<div class="span2 offset1 grey">
+						<p>Chapel Hill, NC</p>
+						<p>2007-2011</p>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="span5 offset3">
+						<p>Christ School</p>
+						<p style="font-style: italic;">National Honors Society</p>
+						<p style="font-style: italic;">Salutatorian</p>
+					</div>
+					<div class="span2 offset1 grey">
+						<p>Arden, NC</p>
+						<p>2003-2007</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span2">
+						<div class="underline">SKILLS</div>
+					</div>
+					<div class="span3 offset1">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>HTML5</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 90%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>JavaScript/jQuery</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 80%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>CSS3</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 80%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>AJAX</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 70%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+					</div>
+					<div class="span3 offset1">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>PHP</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 70%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>WordPress</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 90%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>SASS/LESS</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 80%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>Python</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 50%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+					</div>
+				</div>
+				<div class="row">
+					<div class="span3 offset3">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>Google Analytics</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 80%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>SPSS</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 60%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>SAS</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 40%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>Excel</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 80%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+					</div>
+					<div class="span3 offset1">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>Photoshop</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 70%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>Illustrator</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 60%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>Premiere</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 80%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+						<div class="skill">
+							<div class="floatLeft">
+								<p>After Effects</p>
+							</div>
+							<div class="floatRight progress skillbar">
+								<div class="bar" style="width: 70%;"></div>
+							</div>
+						</div>
+						<br class="clearfix clear">
+					</div>
+				</div>
+			</div>
+		</div><!--RESUME-->
+
+		<div id="portfolio" style="height:720px;background-color: #efefef;">
+			<div class="container" style="max-width:1240px;margin:0 auto;">
+				<br/>
+				<div class="row">
+					<div class="span2">
+						<div class="underline">PORTFOLIO</div>	
+					</div>
+
+					<div class="span9" style="padding-top:0;">
+						<iframe class="folio" scrolling="no" style="width:inherit;height:610px;border:none;overflow-y:hidden;" src="http://eschie.carbonmade.com/"></iframe>
+					</div>
+				</div>
+			</div>
+		</div><!--PORTFOLIO-->
+
+		<div id="contact" style="min-height:600px;background-color: black;">
+			<div class="container" style="max-width:1240px;margin:0 auto;">
+				<br/><br/>
+				<div class="row" style="color:white;">
+					<div class="span2">
+						<div class="underline">CONTACT</div>	
+					</div>
+
+					<div class="span5 offset1">
+						
+						<?php if(isset($hasError)) { //If errors are found ?>
+							<p class="error">Please check if you've filled all the fields with valid information. Thank you.</p>
+						<?php } ?>
+
+						<?php if(isset($emailSent) && $emailSent == true) { //If email is sent ?>
+							<p style="color:#0e90d2;"><strong>Email Successfully Sent!</strong></p>
+							<p>Thanks, <strong><?php echo $name;?></strong>, for your message! Your email was successfully sent and I will be in touch with you soon.</p>
+						<?php } ?>
+
+						<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform">
+							<div>
+							    <label for="name"><strong>Name:</strong></label>
+								<input type="text" style="width:100%;" name="contactname" id="contactname" value="" class="required" />
+							</div>
+
+							<div>
+								<label for="email"><strong>Email:</strong></label>
+								<input type="text" style="width:100%;" name="email" id="email" value="" class="required email" />
+							</div>
+
+							<div>
+								<label for="subject"><strong>Subject:</strong></label>
+								<input type="text" style="width:100%;" name="subject" id="subject" value="" />
+							</div>
+
+							<div>
+								<label for="message"><strong>Message:</strong></label>
+								<textarea rows="5" style="width:100%;" name="message" id="message"></textarea>
+							</div>
+						    <input type="submit" value="Send Message" name="submit" />
+						</form>
+					</div>
+				</div>
+			</div>
+			<div style="margin-top:20px;margin-left:20px;height:80px;color:#999;text-align:left;">Austin Eschenbach | &copy; 2013</div>
+		</div><!--CONTACT-->
+
+		<div class="navbar navbar-inverse navbar-fixed-bottom" style="position:fixed;">
+			<div class="navbar-inner" style="text-align:center;">
+				<ul class="nav">
+					<!--<li><a href="https://github.com/eschie" target="about:blank"><img src="img/github.png"></a></li>-->
+					<li><a href="http://www.facebook.com/austin.eschenbach" target="about:blank"><img src="img/small/fb.png"></a></li>
+					<li><a href="https://plus.google.com/101803075259385117808/post" target="about:blank"><img src="img/small/gplus.png"></a></li>
+					<li><a href="http://www.twitter.com/aeschenbach" target="about:blank"><img src="img/small/twt.png"></a></li>
+					<li><a href="skype:austin.eschenbach?call" target="about:blank"><img src="img/small/skype.png"></a></li>
+					<li><a href="http://www.linkedin.com/in/aeschenbach" target="about:blank"><img src="img/small/lnkdin.png"></a></li>
+				</ul>
+			</div>
+		</div><!--BOTTOM-NAVBAR-->
+
+		<script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/jquery.scrollTo-min.js"></script>
+		<script src="plugins/validation/dist/jquery.validate.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$("#contactform").validate();
+
+				$('.navbar').scrollspy();
+
+				$('.nav li a.main-nav').bind('click', function(event) {
+					event.preventDefault();
+					target = this.hash;
+					console.log(target);
+					$.scrollTo(target, 700, {offset:-40});
+				});
+			});
+		</script>
+		<script type="text/javascript">
+			(function(){
+				var _emailSent = "<?php echo $emailSent;?>";
+				console.log(_emailSent);
+				if(_emailSent == true){
+					$('body').scrollTo('#contact',700);
+				}
+			})();
+		</script>
+	</body>
+
+</html>
